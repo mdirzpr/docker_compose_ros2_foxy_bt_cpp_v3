03094f3a8db11f276c8fbecc9e5fa4a79108cd9a
Mahdi <57127950+mdirzpr@users.noreply.github.com>
2023-09-04 16:34:20 +0200

    Update build_all.sh

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -7,7 +7,7 @@ TARGET_ARCH=$1
 echo --- clean up Docker
 bash docker_clean.sh
 echo TARGET_ARCH: ${TARGET_ARCH}
-echo --- build ros2_foxy_bt
-bash build_ctrlx_ros2_foxy_bt.sh ${TARGET_ARCH}
+echo --- build ros2_foxy_bt_v3
+bash build_ctrlx_ros2_foxy_bt_v3.sh ${TARGET_ARCH}
 echo --- build snap
 bash build_snap.sh ${TARGET_ARCH}
